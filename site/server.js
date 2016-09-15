c9f95555bf6fbfea83f93c37a5aad018351db99a
fottua <fott.ua@gmail.com>
2016-09-15 20:27:25 +0300

    Update server.js

--- site/server.js
+++ site/server.js
@@ -3,8 +3,11 @@ var app = express();
 var server = require('http').createServer(app);
 var port = process.env.PORT || 8080;
 server.listen(port);
-var request = server.get("http://stats.zello.com/channels-suggest/ru/")
 
+
+app.get('/', function(req, res){
+  res.send('hello world');
+});
 var request = server.get("http://stats.zello.com/channels-suggest/ru/", function(response) {
   response.send(request);
 });
