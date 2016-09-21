c8062a91eb80ead32f901581a3f0fe78d133cd45
fottua <fott.ua@gmail.com>
2016-09-21 10:10:52 +0300

    Update server.js

--- site/server.js
+++ site/server.js
@@ -1,22 +1,59 @@
 var app = require('express')();
 var server = require('http').createServer(app);
-
+var io = require('socket.io').listen(server)
 var port = process.env.PORT || 80;
+var Client = require('node-rest-client').Client;
 server.listen(port);
-var obj={};
-var unirest = require('unirest');
+var client = new Client();
+ var obj = {};
+ var _ = require('underscore');
+// direct way 
+client.get("http://stats.zello.com/channels-suggest/ua", function (data, response) {
+    // parsed response body as js object 
+    
+    
 
-unirest.get("http://stats..com/channels-suggest/ru")
-  .send()
-  .end(response=> {
-    if (response.ok) {
-      
-	  obj = JSON.parse(response.raw_body);
-	  return obj;
-    } else {
-      console.log("Got an error: ", response.error)
-    }
-  })
-  app.get('/', function (req, res) {
-  res.send(obj);
+    //for (var key in data) {
+      for (i = 0; i < data.length; ++i){
+        obj[i] = _.pick(data[i], 'channel');
+        obj[i].channel_name = data[i].channel;//
+        obj[i].on_devices = data[i].devices_online;//
+        obj[i].admin = data[i].owner_name;//
+        obj[i].user = data[i].users_online;//
+        obj[i].description = data[i].channel_description;//
+        obj[i].picture = data[i].profile.picture;//
+        obj[i].voice = data[i].profile.voice;
+        obj[i].moder = data[i].moderators;
+        delete obj[i].channel;
+        
+      }
+    //}; 
+    // raw response 
+});
+
+
+
+
+
+
+ // });
+
+app.get('/', function(req, res){
+  res.sendfile(__dirname + '/index.html');
+});
+
+io.on('connection', function (socket) {
+  socket.emit('news', obj);
+  socket.on('my other event', function (obj) {
+    
+  });
+});
+
+io.on('connection', function(socket){
+  
+  
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+    io.emit('chat message', obj);
+  });
 });
